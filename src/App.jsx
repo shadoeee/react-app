bc4fce753034d3be2e4347222e924962898a6cea
shadoeee <balaganesh541@gmail.com>
2023-10-16 16:59:54 +0530

    adding first card

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
-function App() {
+import './styles/App.css'
+
+function App({card1}) {
+
+    console.log(card1);
+
     return (
-        <div>
-            Hello World!
+        <div className="card">
+            <h1>Price Card Task</h1>
+            <ul>
+                {
+                    card1.map(card1 =>
+                        <li key={card1.id}>{ card1.content }</li>)
+                }
+            </ul>
         </div>
     )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,58 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+//data
+
+const card1 = [
+    {
+        id: 1,
+        content: 'Single User',
+        important: true
+    },
+
+    {
+        id: 2,
+        content: 'Single User',
+        important: true
+    },
+
+    {
+        id: 3,
+        content: 'Single User',
+        important: true
+    },
+
+    {
+        id: 4,
+        content: 'Single User',
+        important: true
+    },
+
+    {
+        id: 5,
+        content: 'Single User',
+        important: false 
+    },
+
+    {
+        id: 6,
+        content: 'Single User',
+        important: false
+    },
+
+    {
+        id: 7,
+        content: 'Single User',
+        important: false
+    },
+
+    {
+        id: 8,
+        content: 'Single User',
+        important: false
+    }
+]
+
 ReactDOM.createRoot(document.getElementById('root')).render( <
-    App / >
+    App card1={ card1 } / >
 );
