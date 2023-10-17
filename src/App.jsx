0054b63325b0c3778b1ad971317d3e6dcddeaa7a
shadoeee <balaganesh541@gmail.com>
2023-10-17 16:21:42 +0530

    worked on simple increment, decrement and rest class component

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,43 @@
-import './styles/App.css'
-import FeaturesList from './components/FeatureList';
+import React, { Component } from 'react'
 
+class App extends Component {
 
-function App({ card1 }) {
-    const cardIdWithCustomButton = 3;
-  return (
-    <div className="cards-container">
-      {card1.map((card) => (
-        <div key={card.id} className="card">
-          <h3 className="centered-text1">{card.title}</h3>
-          <h1 className="centered-text2">{`$${card.price}/month`}</h1>
-          <ul id='list'>
-            {card.features.map((feature) => (
-                <FeaturesList key={feature.id} feature={ feature } />
-            ))}
-              </ul>
-             {card.id === cardIdWithCustomButton ? (
-            <button className="custom-button">Button</button>
-          ) : (
-            <button className="standard-button">Button</button>
-          )}
-        </div>
-      ))}
-    </div>
-  );
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      count: 0,
+    }
+  }
+
+  increment = () => {
+    this.setState({
+      count: this.state.count + 1,
+    })
+  }
+
+   decrement = () => {
+    this.setState({
+      count: this.state.count - 1,
+    })
+   }
+  
+   reset = () => {
+    this.setState({
+      count: 0,
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <p>Count: { this.state.count }</p>
+        <button onClick={this.increment}>Increment</button>
+        <button onClick={this.decrement}>Decrement</button>
+        <button onClick={this.reset}>Reset</button>
+      </div>
+    )
+  }
 }
 
 export default App
--- src/main.jsx
+++ src/main.jsx
@@ -170,58 +170,8 @@ const cardsData = [
 ];
 
 
-// const card1 = [
-//     {
-//         id: 1,
-//         content: 'Single User',
-//         important: true
-//     },
-
-//     {
-//         id: 2,
-//         content: '50GB Storage',
-//         important: true
-//     },
-
-//     {
-//         id: 3,
-//         content: 'Unlimited Public Projects',
-//         important: true
-//     },
-
-//     {
-//         id: 4,
-//         content: 'Community Access',
-//         important: true
-//     },
-
-//     {
-//         id: 5,
-//         content: 'Unlimited Private Projects',
-//         important: false 
-//     },
-
-//     {
-//         id: 6,
-//         content: 'Dedicated Phone Support',
-//         important: false
-//     },
-
-//     {
-//         id: 7,
-//         content: 'Free Subdomain',
-//         important: false
-//     },
-
-//     {
-//         id: 8,
-//         content: 'Monthly Status Reports',
-//         important: false
-//     }
-// ]
-
 
 
 ReactDOM.createRoot(document.getElementById('root')).render( <
-    App card1={cardsData}  / >
+    App / >
 );
