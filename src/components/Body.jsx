5b62fc21dbe04eda42ad384d4ce70e81c0d8cd03
shadoeee <balaganesh541@gmail.com>
2023-10-18 01:36:36 +0530

    working on UI design

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -10,19 +10,18 @@ function Body() {
               <h1>Shop in style</h1>
               <h3>with this shop homepage template</h3>
           </div>
-          <div>
+          <div className='card-design1'>
               <Card style={{ width: '18rem' }}>
-                  <h5>sale</h5>
+                  
                   <figure>
       <Card.Img variant="top" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.78DZBgvwgpcB00yi7BPZBgHaFi%26pid%3DApi&f=1&ipt=32864e04578468dbf013383858f8afd202ebd2d42b9765c7920da3bcb0763722&ipo=images" alt='image'></Card.Img><figcaption>450 X 300</figcaption>
       </figure>
         <Card.Body>
-        <Card.Title>Card Title</Card.Title>
+        <Card.Title>Fancy Product</Card.Title>
         <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
+          $40.00 - $80.00
         </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
+        <Button variant="outline-dark">View options</Button>
       </Card.Body>
               </Card>
               
@@ -37,7 +36,7 @@ function Body() {
           Some quick example text to build on the card title and make up the
           bulk of the card's content.
         </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
+        <Button variant="outline-dark">View options</Button>
       </Card.Body>
               </Card>
               
@@ -52,7 +51,84 @@ function Body() {
           Some quick example text to build on the card title and make up the
           bulk of the card's content.
         </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
+        <Button variant="outline-dark">View options</Button>
+      </Card.Body>
+              </Card>
+              
+               <Card style={{ width: '18rem' }}>
+                  
+                  <figure>
+      <Card.Img variant="top" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.78DZBgvwgpcB00yi7BPZBgHaFi%26pid%3DApi&f=1&ipt=32864e04578468dbf013383858f8afd202ebd2d42b9765c7920da3bcb0763722&ipo=images" alt='image'></Card.Img><figcaption>450 X 300</figcaption>
+      </figure>
+        <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Button variant="outline-dark">View options</Button>
+      </Card.Body>
+    </Card>
+          </div>
+
+          <div className='card-design1'>
+              <Card style={{ width: '18rem' }}>
+                  <h5>sale</h5>
+                  <figure>
+      <Card.Img variant="top" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.78DZBgvwgpcB00yi7BPZBgHaFi%26pid%3DApi&f=1&ipt=32864e04578468dbf013383858f8afd202ebd2d42b9765c7920da3bcb0763722&ipo=images" alt='image'></Card.Img><figcaption>450 X 300</figcaption>
+      </figure>
+        <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Button variant="outline-dark">View options</Button>
+      </Card.Body>
+              </Card>
+              
+              <Card style={{ width: '18rem' }}>
+                  
+                  <figure>
+      <Card.Img variant="top" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.78DZBgvwgpcB00yi7BPZBgHaFi%26pid%3DApi&f=1&ipt=32864e04578468dbf013383858f8afd202ebd2d42b9765c7920da3bcb0763722&ipo=images" alt='image'></Card.Img><figcaption>450 X 300</figcaption>
+      </figure>
+        <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Button variant="outline-dark">View options</Button>
+      </Card.Body>
+              </Card>
+              
+              <Card style={{ width: '18rem' }}>
+                  <h5>sale</h5>
+                  <figure>
+      <Card.Img variant="top" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.78DZBgvwgpcB00yi7BPZBgHaFi%26pid%3DApi&f=1&ipt=32864e04578468dbf013383858f8afd202ebd2d42b9765c7920da3bcb0763722&ipo=images" alt='image'></Card.Img><figcaption>450 X 300</figcaption>
+      </figure>
+        <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Button variant="outline-dark">View options</Button>
+      </Card.Body>
+              </Card>
+              
+               <Card style={{ width: '18rem' }}>
+                  
+                  <figure>
+      <Card.Img variant="top" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.78DZBgvwgpcB00yi7BPZBgHaFi%26pid%3DApi&f=1&ipt=32864e04578468dbf013383858f8afd202ebd2d42b9765c7920da3bcb0763722&ipo=images" alt='image'></Card.Img><figcaption>450 X 300</figcaption>
+      </figure>
+        <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Button variant="outline-dark">View options</Button>
       </Card.Body>
     </Card>
           </div>
