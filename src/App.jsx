b46506cfab93a43bf3314fd8265e788ec353a9ec
shadoeee <balaganesh541@gmail.com>
2023-10-18 22:46:45 +0530

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
-import Header from './components/header';
+import Header from './components/Header';
 import Body from './components/Body';
 import Footer from './components/Footer';
 
