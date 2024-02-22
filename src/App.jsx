66556a416e87877e08db375233748225e82daa87
shadoeee <109212051+shadoeee@users.noreply.github.com>
2024-02-22 20:02:00 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,29 @@
-import React, { useState } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.min.js';
-import Header from './components/header';
-import Body from './components/Body';
-import Footer from './components/Footer';
+import './styles/App.css'
+import FeaturesList from './components/FeatureList';
 
 
-function App() {
-  const [cartCount, setCartCount] = useState(0);
-
-  // Function to update the cart count
-  const updateCartCount = (increment) => {
-    setCartCount(cartCount + increment);
-  };
-
+function App({ card1 }) {
+    const cardIdWithCustomButton = 3;
   return (
-    <div>
-      <Header cartCount={cartCount} />
-      <Body updateCartCount={updateCartCount} />
-      <Footer />
+    <div className="cards-container">
+      {card1.map((card) => (
+        <div key={card.id} className="card">
+          <h3 className="centered-text1">{card.title}</h3>
+          <h1 className="centered-text2">{`$${card.price}/month`}</h1>
+          <ul id='list'>
+            {card.features.map((feature) => (
+                <FeaturesList key={feature.id} feature={ feature } />
+            ))}
+              </ul>
+             {card.id === cardIdWithCustomButton ? (
+            <button className="custom-button">Button</button>
+          ) : (
+            <button className="standard-button">Button</button>
+          )}
+        </div>
+      ))}
     </div>
   );
 }
-
 
 export default App
